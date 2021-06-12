6d8d2cc1f624edc77fafb0296e056a9014acc146
Shabbirsheikh <41716588+Shabbirsheikh@users.noreply.github.com>
2021-06-12 22:22:10 +0530

    Add files via upload

---
+++ lcm_and_hcf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	printf("Enter two numbers\n");
+	scanf("%d %d",&a,&b);
+	int hcf,lcm;
+	int i;
+	for(i=1;i<=a||i<=b;i++){
+		if(a%i==0&&b%i==0){
+			hcf=i;
+		}
+	}
+	lcm=(a*b)/hcf;//because we know that lcm*hcf=product of two numbers
+	printf("HCF and LCM of two numbers %d and %d HCF=%d,LCM=%d",a,b,hcf,lcm);
+	return 0;
+}
